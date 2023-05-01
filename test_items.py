9a7296f0b188b11119c45305984d2d97f83694a9
Yuri Buzhenko <yuri0291@gmail.com>
2023-05-02 01:08:44 +0700

    Slightly changed the assert of the chec.

--- test_items.py
+++ test_items.py
@@ -7,6 +7,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_find_cart_button(browser):
     browser.get(link)
     time.sleep(5)
-    cart_button = browser.find_element(By.CLASS_NAME, "btn.btn-lg.btn-primary")
+    cart_button = len(browser.find_elements(By.CLASS_NAME, "btn.btn-lg.btn-primary"))
 
-    assert cart_button.is_displayed(), "Add to cart button is not found / Кнопка ДОБАВИТЬ В КОРЗИНУ не найдена"
+    assert cart_button > 0, "Add to cart button is not found / Кнопка ДОБАВИТЬ В КОРЗИНУ не найдена"
